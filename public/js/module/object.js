ce5b0b538b213c8f5c83405ad54f529501d5f674
Hndrckx <90184045+Hndrckx@users.noreply.github.com>
2021-12-08 12:17:11 +0100

    Perso DONE AH

--- public/js/module/object.js
+++ public/js/module/object.js
@@ -1,10 +1,34 @@
 let perso = {
-nom = "Michel",
-lieu = "la maison",
-argent = 85,
-mainDroite = [],
-mainGauche = [],
-seDéplacer =
+    nom: "Michel",
+    lieu: "la maison",
+    argent: 85,
+    mainDroite: [],
+    mainGauche: [],
+    seDéplacer: function (a) {
+        a.push(perso)
+        perso.lieu = a.nom
+        alert((perso.nom) + " est à " + (perso.lieu))
+    },
+    payer: function (b) {
+        b.forEach(item => {
+            perso.argent - item.prix
+        });
+    },
+    couper: function (c, d) {
+        c.forEach(item => {
+            if (item.etat == "entier") {
+                item.etat = "coupé";
+                d.push(item)
+                alert((item.nom) + "a été coupé et mis dans le bol.")
+            } else {
+                d.push(item)
+                alert((item.nom) + "a été mis dans le bol.")
+            }
+        })
+    }
+}
 
-
+let home = {
+    nom: "la maison",
+    personnes: [perso],
 }
